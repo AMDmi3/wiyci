7a58be7771b74564edfd8df6c090a0045f9791de
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-07-07 15:22:31 +0300

    parser: Make coverage_attribute conditional in cfg(test), like in other crates

--- wiyci-parser/src/lib.rs
+++ wiyci-parser/src/lib.rs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Copyright 2026 Dmitry Marakasov <amdmi3@amdmi3.ru>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#![feature(coverage_attribute)]
+#![cfg_attr(test, feature(coverage_attribute))]
 
 #[macro_use]
 mod typed_storage;
