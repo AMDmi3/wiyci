a14c6c415d73bf5983ff79e7c4f721f66ae6aeb3
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-07-16 20:48:20 +0300

    web: Don't limit number of projects shown until we have pagination

--- wiyci-web/src/constants.rs
+++ wiyci-web/src/constants.rs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: Copyright 2026 Dmitry Marakasov <amdmi3@amdmi3.ru>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pub const PROJECTS_PER_PAGE: u64 = 50;
+//pub const PROJECTS_PER_PAGE: u64 = 50;
+pub const PROJECTS_PER_PAGE: u64 = 1000; // until pagination is implemented
