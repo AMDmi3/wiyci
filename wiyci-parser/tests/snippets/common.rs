ea4495fe29b2b4084535dd4bf18d510fbc37ca88
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-07-17 23:07:44 +0300

    parser: Reorganize tests

---
+++ wiyci-parser/tests/snippets/common.rs
@@ -0,0 +1,43 @@
+// SPDX-FileCopyrightText: Copyright 2026 Dmitry Marakasov <amdmi3@amdmi3.ru>
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+use std::fmt::Debug;
+use std::io::{BufReader, Cursor};
+use std::ops::ControlFlow;
+
+use itertools::Itertools as _;
+
+use wiyci_parser::{LogParser, SnippetHandler, snippets::*};
+
+#[derive(Default)]
+struct SnippetSaver {
+    snippets: Vec<Snippet>,
+}
+
+impl SnippetHandler for SnippetSaver {
+    fn handle_snippet(&mut self, snippet: Snippet) -> ControlFlow<()> {
+        self.snippets.push(snippet);
+        ControlFlow::Continue(())
+    }
+}
+
+#[track_caller]
+pub fn parse_snippet<T>(text: &str) -> T
+where
+    T: Debug + TryFrom<Snippet>,
+    <T as TryFrom<Snippet>>::Error: Debug,
+{
+    let mut saver = SnippetSaver::default();
+
+    LogParser::default()
+        .parse(BufReader::new(Cursor::new(text)), &mut saver)
+        .expect("parsing failed");
+
+    saver
+        .snippets
+        .into_iter()
+        .exactly_one()
+        .expect("parser was expected to return exactly one snippet")
+        .try_into()
+        .expect("got unexpected snippet type")
+}
