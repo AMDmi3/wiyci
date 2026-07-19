a0c6c780cb56acc083b2ede1f2186516d99bdccd
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-07-19 19:47:57 +0300

    web: Bump projects listing limit

--- wiyci-web/src/constants.rs
+++ wiyci-web/src/constants.rs
@@ -2,4 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 //pub const PROJECTS_PER_PAGE: u64 = 50;
-pub const PROJECTS_PER_PAGE: u64 = 1000; // until pagination is implemented
+pub const PROJECTS_PER_PAGE: u64 = 2500; // until pagination is implemented
