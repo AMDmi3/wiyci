a7fdd6507e29cefe0fb75cd2b67fb43d9f6e74b4
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-07-20 14:46:25 +0300

    daemon: Make function ptr arguments and generics more apparent

--- wiyci-daemon/src/workers/util.rs
+++ wiyci-daemon/src/workers/util.rs
@@ -13,27 +13,27 @@ const DEFAULT_TASK_WAIT: Duration = if cfg!(debug_assertions) {
 };
 const DEFAULT_RETRY_WAIT: Duration = Duration::from_secs(60);
 
-pub struct PollingWorkerRunner<Task, GetTask, ProcessTask> {
+pub struct PollingWorkerRunner<Task, GetTaskFn, ProcessTaskFn> {
     name: &'static str,
-    get_task: GetTask,
-    process_task: ProcessTask,
-    span_from_task: Option<fn(&Task) -> Span>,
+    get_task_fn: GetTaskFn,
+    process_task_fn: ProcessTaskFn,
+    span_from_task_fn: Option<fn(&Task) -> Span>,
     task_wait: Duration,
     retry_wait: Duration,
 }
 
-impl<Task, Error, GetTask, ProcessTask> PollingWorkerRunner<Task, GetTask, ProcessTask>
+impl<Task, Error, GetTaskFn, ProcessTaskFn> PollingWorkerRunner<Task, GetTaskFn, ProcessTaskFn>
 where
     Error: std::fmt::Display,
-    GetTask: AsyncFn() -> Result<Option<Task>, Error>,
-    ProcessTask: AsyncFn(&Task) -> Result<(), Error>,
+    GetTaskFn: AsyncFn() -> Result<Option<Task>, Error>,
+    ProcessTaskFn: AsyncFn(&Task) -> Result<(), Error>,
 {
-    pub fn new(name: &'static str, get_task: GetTask, process_task: ProcessTask) -> Self {
+    pub fn new(name: &'static str, get_task_fn: GetTaskFn, process_task_fn: ProcessTaskFn) -> Self {
         Self {
             name,
-            get_task,
-            process_task,
-            span_from_task: None,
+            get_task_fn,
+            process_task_fn,
+            span_from_task_fn: None,
             task_wait: DEFAULT_TASK_WAIT,
             retry_wait: DEFAULT_RETRY_WAIT,
         }
@@ -52,19 +52,19 @@ where
     }
 
     #[allow(unused)]
-    pub fn with_span(mut self, span_from_task: fn(&Task) -> Span) -> Self {
-        self.span_from_task = Some(span_from_task);
+    pub fn with_span(mut self, span_from_task_fn: fn(&Task) -> Span) -> Self {
+        self.span_from_task_fn = Some(span_from_task_fn);
         self
     }
 
     pub async fn run(&self) -> Result<(), Error>
     where
         Error: std::fmt::Display,
-        GetTask: AsyncFn() -> Result<Option<Task>, Error>,
-        ProcessTask: AsyncFn(&Task) -> Result<(), Error>,
+        GetTaskFn: AsyncFn() -> Result<Option<Task>, Error>,
+        ProcessTaskFn: AsyncFn(&Task) -> Result<(), Error>,
     {
         loop {
-            let task = match (self.get_task)().await {
+            let task = match (self.get_task_fn)().await {
                 Err(error) => {
                     counter!("wiyci_daemon_worker_runs_total", "worker" => self.name, "status" => "failure").increment(1);
                     error!(%error, "error while polling for task");
@@ -82,13 +82,13 @@ where
 
             let start = Instant::now();
             let span = self
-                .span_from_task
+                .span_from_task_fn
                 .as_ref()
                 .map(|f| f(&task))
                 .unwrap_or_else(Span::none);
             let res = async {
                 debug!("start processing");
-                let res = (self.process_task)(&task).await;
+                let res = (self.process_task_fn)(&task).await;
 
                 let duration = Instant::now()
                     .saturating_duration_since(start)
